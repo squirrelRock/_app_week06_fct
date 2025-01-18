478b9cf44f0a49e1b7f362ac44e7f12205e35520
Kelly Durkin <kdurkin@bearcubs.santarosa.edu>
2025-01-18 14:01:59 -0800

    returned to original layout

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,21 @@ import Layout from '../components/Layout';
 // }
 
 // HOME COMPONENT
-export default function Home() {
+export default function Home({ allDataMain }) {
   return (
     <Layout>
  
+      <h4 className="mt-3 text-center">Shōgun Characters</h4>
+<div className="d-flex flex-column align-items-center mb-5">
 
+  <Link href="/things" className="btn btn-secondary btn-sm small mt-2 text-center">
+  Character Lists
+  </Link>
+  <Link href="/clans" className="btn btn-secondary btn-sm small mt-2 text-center">
+    Clans and Bonded Relationships
+  </Link>
+ 
+</div>
 <hr/>
 <h6 className='mt-4'><strong>Sept 16, 2024</strong></h6>
        <div className='border p-3 mt-1'>
@@ -26,7 +36,7 @@ export default function Home() {
       <p className="display-6 p-5"> <strong className="display-4">Shōgun</strong> - &#8220;An adaptation from a 1975 best-selling book centered on 17th century feudal Japan on the brink of civil war, set a record on September 15, 2024 for <strong>most Emmys won by a show in a single year, winning 18 in all</strong>...&#8220; <strong>- The New York Times</strong></p>
       </div>
       </div>
-      <hr/>
+
     </Layout>
   );
 }
